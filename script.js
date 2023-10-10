0a1688d9c8cd0abfa008ab5e4c8a253b900beb4f
Prem-Pandey <dev.premrpandey@gmail.com>
2023-10-10 16:09:37 +0530

    QuerySelector-tut

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-var items = document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[2])
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[2])
 // items[1].textContent = 'hello'
 // items[1].style.fontWeight = 'bold'
-items[2].style.backgroundColor = 'green'
+// items[2].style.backgroundColor = 'green'
 
-for(var i =0; i<items.length; i++)
+// for(var i =0; i<items.length; i++)
+// {
+//     items[i].style.fontWeight = 'bold'
+// }
+// var list = document.getElementsByTagName('li')
+// var list5 = document.getElementsByClassName('item5')
+// list[4].style.backgroundColor = 'red'
+// list[4].style.fontWeight = 'bold'
+// list[4].style.border = '2px solid black'
+// list5[0].style.borderRadius = '10px'
+
+// QUERRY SELECTOR
+
+var item  = document.querySelector('.list-group-item:nth-child(2)')
+item.style.backgroundColor = 'green'
+var item3  = document.querySelector('.list-group-item:nth-child(3)')
+item3.style.display = 'none'
+
+// QUERY SELECTOR ALL
+
+var item2 = document.querySelectorAll('li')
+item2[1].style.color=  'green'
+
+var odd = document.querySelectorAll('li:nth-child(odd')
+
+for(var i =0; i<odd.length; i++)
 {
-    items[i].style.fontWeight = 'bold'
+    odd[i].style.backgroundColor = 'green'
 }
-var list = document.getElementsByTagName('li')
-var list5 = document.getElementsByClassName('item5')
-list[4].style.backgroundColor = 'red'
-list[4].style.fontWeight = 'bold'
-list[4].style.border = '2px solid black'
-list5[0].style.borderRadius = '10px'
