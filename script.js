7e2f618a413202ceeafdc8d6290aa4673eeb9801
Prem-Pandey <dev.premrpandey@gmail.com>
2023-10-10 14:54:45 +0530

    ByTagName-tut

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ for(var i =0; i<items.length; i++)
 {
     items[i].style.fontWeight = 'bold'
 }
+var list = document.getElementsByTagName('li')
+var list5 = document.getElementsByClassName('item5')
+list[4].style.backgroundColor = 'red'
+list[4].style.fontWeight = 'bold'
+list[4].style.border = '2px solid black'
+list5[0].style.borderRadius = '10px'
